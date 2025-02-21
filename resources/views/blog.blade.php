2d070984bb50a341f79abc86b1db3c10fcdbf34c
Ilham-Bashthotan <ilhambashthotan123@gmail.com>
2025-02-21 21:43:15 +0700

    Add layout and navigation components, refactor pages to use Blade components

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,16 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Blog Page</title>
-</head>
-<body>
-    <h1>Blog Page</h1>
-    <article>
-        <h2>Post Title</h2>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident ad aperiam, possimus illo recusandae voluptatibus hic, eius voluptatem debitis, nemo consequatur modi repellat odit voluptates optio error maiores quibusdam facilis.</p>
-    </article>
-</body>
-</html>
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot>
+    <h3 class="text-xl">Welcome to my blog</h3>
+</x-layout>
