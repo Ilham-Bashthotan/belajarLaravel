2c62d7577f9d68b35208c5ea76b4f258d16ec8a9
Ilham-Bashthotan <ilhambashthotan123@gmail.com>
2025-02-21 17:18:00 +0700

    Add home and about pages with basic styling and scripts

---
+++ resources/views/about.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>About Page</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <a href="/">home</a>
+    <a href="/about">about</a>
+    <h1>About Page</h1>
+    <h3>Owner saha? {{ $name }}</h3>
+    <img src="img/meme-kucing.jpeg" alt="meme-kucing" width="300">
+</body>
+
+</html>
